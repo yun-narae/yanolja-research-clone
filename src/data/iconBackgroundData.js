c4f05e8a1c0a1f5a69899fefed3a3c0a3695657c
narae <skfo0827@naver.com>
2025-11-17 08:40:13 +0900

    refactor(main): asset 아이콘 정리 및 더미데이터 추가
    - 스프라이트 SVG 정리
    - 이미지 파일명 변경
    - 더미데이터 파일 생성

---
+++ src/data/iconBackgroundData.js
@@ -0,0 +1,32 @@
+// IconBackgroundCard 더미데이터
+import bgChart from "../asset/card_bg/bg_chart.svg";
+import bgGlass from "../asset/card_bg/bg_glass.svg";
+import bgDatalab from "../asset/card_bg/bg_datalab.svg";
+
+export const iconBackgroundData = [
+    {
+        id: "attractiveness-index",
+        title: "야놀자 매력도 지수",
+        linkText: "바로가기",
+        iconName: "bg_chart",
+        iconSrc: bgChart,
+        href: "/",
+    },
+    {
+        id: "brand-asset-index",
+        title: "야놀자 브랜드자산 지수",
+        linkText: "바로가기",
+        iconName: "bg_glass",
+        iconSrc: bgGlass,
+        href: "/",
+    },
+    {
+        id: "datalab",
+        title: "데이터랩",
+        linkText: "바로가기",
+        iconName: "datalab",
+        iconSrc: bgDatalab,
+        href: "/",
+    },
+];
+
